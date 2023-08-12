638d8cb3d50e22d134330c15c5fb4bb9a5177f2a
kalyantol <kalyan.tol>
2023-08-12 18:37:17 +0600

    first commit

---
+++ js/cal.js
@@ -0,0 +1,47 @@
+function getInputTextById(event){
+    const inputText = document.getElementById(event);
+    const inputTextString =inputText.value;
+    const inputAmount = parseFloat(inputTextString);
+    return inputAmount;
+}
+
+function getPTextById(event){
+    const pText = document.getElementById(event);
+    const pTextString =pText.innerText;
+    const pAmount = parseFloat(pTextString);
+    return pAmount;
+}
+
+function setElementTextById(elementId, newValue){
+    const textElement = document.getElementById(elementId);
+    textElement.innerText = newValue;
+}
+
+document.getElementById('calcul-btn').addEventListener('click', function(){
+    const totalIncome = getInputTextById('income-input');
+    const foodAmount = getInputTextById('food-input');
+    const rentAmount = getInputTextById('rent-input');
+    const clothsAmount = getInputTextById('cloths-input');
+
+    
+    const allExpence = foodAmount + rentAmount + clothsAmount;
+    const totalBalence = totalIncome - allExpence;
+    setElementTextById("total-exp", allExpence);
+    setElementTextById('bal-amo', totalBalence);
+})
+
+document.getElementById('save-btn').addEventListener('click', function(){
+    const saveAmount = getInputTextById('save-input');
+    const totalBalence = getPTextById('bal-amo');
+    if(totalBalence <= 0){
+        alert('No Balence')
+        return;
+    }
+    const savingBalence = Math.round((saveAmount*100) / totalBalence).toFixed(3);
+    const remBalence = totalBalence - savingBalence;
+
+    setElementTextById('save-amount', savingBalence);
+    setElementTextById('total-rem-bal', remBalence);
+
+
+})
